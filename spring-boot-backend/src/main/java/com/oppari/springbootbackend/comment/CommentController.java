cf4e19d8056934177f4533c8b3c56572654508dc
Compeee <eero.kaarnalehto@outlook.com>
2023-02-11 23:12:12 +0200

    documentation

--- spring-boot-backend/src/main/java/com/oppari/springbootbackend/comment/CommentController.java
+++ spring-boot-backend/src/main/java/com/oppari/springbootbackend/comment/CommentController.java
@@ -15,31 +15,31 @@ import java.util.List;
 public class CommentController {
     private final CommentService commentService;
 
-    @Operation(summary = "Get a list of all the comments")
+    @Operation(summary = "Get a list of all the comments", description = "Requires you to be authenticated")
     @PreAuthorize("hasAnyAuthority('ADMIN', 'USER')")
     @GetMapping
     public List<Comment> getComments(){
         return commentService.getComments();
     }
-    @Operation(summary = "Create a new comment")
+    @Operation(summary = "Create a new comment", description = "Requires you to be authenticated")
     @PreAuthorize("hasAnyAuthority('ADMIN', 'USER')")
     @PostMapping(path="/{postId}")
     public Comment createComment(@RequestBody Comment comment, @PathVariable Long postId){
         return commentService.createComment(comment, postId);
     }
-    @Operation(summary = "Get all comments for a post")
+    @Operation(summary = "Get all comments for a post", description = "Requires you to be authenticated")
     @PreAuthorize("hasAnyAuthority('ADMIN', 'USER')")
     @GetMapping("/{postId}")
     public List<Comment> getCommentsByPost(@PathVariable Long postId){
         return commentService.getCommentsForPost(postId);
     }
-    @Operation(summary = "Delete a comment")
+    @Operation(summary = "Delete a comment", description = "Users can only delete their own comments, admins can delete any")
     @PreAuthorize("#userId == principal.id OR hasAuthority('ADMIN')")
     @DeleteMapping("/{commentId}")
     public void deleteComment(@RequestParam Long userId, @PathVariable Long commentId){
         commentService.deleteComment(commentId);
     }
-    @Operation(summary = "Edit a comment")
+    @Operation(summary = "Edit a comment", description = "Users can edit their own commends, admin can edit any")
     @PreAuthorize("#editCommentRequest.userId == principal.id OR hasAuthority('ADMIN')")
     @PatchMapping("/{commentId}")
     public void editComment(@PathVariable Long commentId, @RequestBody EditCommentRequest editCommentRequest){
--- spring-boot-backend/src/main/java/com/oppari/springbootbackend/user/UserController.java
+++ spring-boot-backend/src/main/java/com/oppari/springbootbackend/user/UserController.java
@@ -16,28 +16,28 @@ import java.util.Optional;
 public class UserController {
     private final UserService userService;
 
-    @Operation(summary = "Get a list of all registered users")
+    @Operation(summary = "Get a list of all registered users", description = "This endpoint is for admins only")
     @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping
     public List<User> getUsers(){
         return userService.getUsers();
     }
 
-    @Operation(summary = "Get an user by id")
+    @Operation(summary = "Get an user by id", description = "You can only get ur own information as an user, admins can find anyones information")
     @PreAuthorize("#userId == principal.id OR hasAuthority('ADMIN')")
     @GetMapping("/{userId}")
     public Optional<User> getUserById(@PathVariable Long userId){
         return userService.findUserById(userId);
     }
 
-    @Operation(summary = "Delete an user")
+    @Operation(summary = "Delete an user", description = "Users can only delete their own accounts, admin can delete anyones")
     @PreAuthorize("#userId == principal.id OR hasAuthority('ADMIN')")
     @DeleteMapping(path = "/{userId}")
     public void deleteUser(@PathVariable("userId") Long userId) {
         userService.deleteUser(userId);
     }
 
-    @Operation(summary = "Change password")
+    @Operation(summary = "Change password", description = "Users can change their own passwords")
     @PreAuthorize("#userId == principal.id")
     @PatchMapping(path = "/{userId}")
     public void changeUserPassword(@PathVariable("userId") Long userId, @RequestBody ChangePassRequest changePassRequest){
